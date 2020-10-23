a9c83775c0be1ee90a6a88992e1d0eeeb1cfacee
Michael Mahlberg <mm@consulting-guild.de>
2020-10-23 15:22:50 +0200

    Statistics are displeyed even if some item's history is empty

--- kanbantesting/kanban.js
+++ kanbantesting/kanban.js
@@ -1,5 +1,5 @@
 const APP_ID = '3074457348136685529'
-const VERSION = '0.0.55'
+const VERSION = '0.0.56'
 const KANBAN = {
     WORKITEM: 'kanbanworkitem',
     STAGE: 'kanbanstage',
--- kanbantesting/kanbanMetrics.js
+++ kanbantesting/kanbanMetrics.js
@@ -17,19 +17,25 @@ function collectHistoryTransitionsFromWorkItems(items) {
     transitionsList = [];
     items.forEach(item => {
         lastStage = "NewItem";
-        item.metadata[APP_ID]['history'].forEach(historyEntry => {
+        if (itemHasHistory(item)) {
+            item.metadata[APP_ID]['history'].forEach(historyEntry => {
 
-            transition = item['id'] + ";" + lastStage + ";" +
-                historyEntry['stage'] + ";" +
-                historyEntry['timestamp'] + ";(" +
-                item['plainText'] + " " +
-                historyEntry['readableTime'] + ")";
+                transition = item['id'] + ";" + lastStage + ";" +
+                    historyEntry['stage'] + ";" +
+                    historyEntry['timestamp'] + ";(" +
+                    item['plainText'] + " " +
+                    historyEntry['readableTime'] + ")";
 
-            lastStage = historyEntry.stage;
-            transitionsList.push(transition);
-        });
+                lastStage = historyEntry.stage;
+                transitionsList.push(transition);
+            });
+        }
     });
     return transitionsList
+}
+
+function itemHasHistory(item) {
+    return item.metadata[APP_ID] && item.metadata[APP_ID]['history']
 }
 
 function CsvRenderer(headlineItems, lines, quoteHints) {
@@ -96,30 +102,38 @@ function CsvRenderer(headlineItems, lines, quoteHints) {
 function KanbanCsvRenderer(itemsList) {
     var itemsList;
     const APP_ID = "3074457348136685529";
+    var csvItemsList = [];
 
     function render() {
         var headlineItems = ["ID", "From Stage", "To Stage", "Timestamp", "plain Text and readable Time"];
         var quotings = [false, true, true, false, true];
-        var csvItemsList = [];
 
         itemsList.forEach(item => {
-            lastStage = "NewItem";
-            item.metadata[APP_ID]['history'].forEach(historyEntry => {
-                var csvItem = [];
-                csvItem.push(item['id']);
-                csvItem.push(lastStage);
-                csvItem.push(historyEntry['stage'])
-                csvItem.push(historyEntry['timestamp'])
-                csvItem.push("(" + item['plainText'] + " " + historyEntry['readableTime'] + ")");
-
-                lastStage = historyEntry.stage;
-                csvItemsList.push(csvItem);
-            });
+            if (hasHistory(item)) {
+                addHistoryEntriesToCsvEntryList(item);
+            }
         });
-
-
         var csvRenderer = new CsvRenderer(headlineItems, csvItemsList, quotings)
         return csvRenderer.render();
+    }
+
+    function addHistoryEntriesToCsvEntryList(item) {
+        lastStage = "NewItem";
+        item.metadata[APP_ID]['history'].forEach(historyEntry => {
+            var csvItem = [];
+            csvItem.push(item['id']);
+            csvItem.push(lastStage);
+            csvItem.push(historyEntry['stage'])
+            csvItem.push(historyEntry['timestamp'])
+            csvItem.push("(" + item['plainText'] + " " + historyEntry['readableTime'] + ")");
+
+            lastStage = historyEntry.stage;
+            csvItemsList.push(csvItem);
+        });
+    }
+
+    function hasHistory(item) {
+        return item.metadata[APP_ID] && item.metadata[APP_ID]['history']
     }
 
     return {
