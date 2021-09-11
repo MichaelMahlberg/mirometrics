fd73c7ec052a88e16a7085551579f0b8b0aaf951
Falk Kühnel <falk@kuehnel.eu>
2021-09-11 14:25:49 +0200

    development Icons now have red borders and strokes

--- flowmetrics/initialize.js
+++ flowmetrics/initialize.js
@@ -1,10 +1,24 @@
 console.log("included initialize file")
 
+colors = {
+    default: 'currentColor',
+    dark: '#555',
+    medium: '#999'
+}
+
+colorsDev = {
+    default: 'red',
+    dark: 'red',
+    medium: 'red'
+}
+
 miro.onReady(() => {
     console.log('Flowmetrics: ' + VERSION)
+    console.log('DEV: ' + DEV)
+    const colorScheme = DEV ? colorsDev : colors;
     const svgIcon = `
     <svg viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg">
-    <rect x="2" y="2" width="44" height="44" rx="4" stroke-width="4" stroke="currentColor" fill="none" />
+    <rect x="2" y="2" width="44" height="44" rx="4" stroke-width="4" stroke="${colorScheme.default}" fill="none"/>
     <line x1="6" y1="11" x2="42" y2="11" stroke-width="2" stroke="currentColor"/>
     <line x1="17" y1="12" x2="17" y2="36" stroke-width="2" stroke="currentColor"/>
     <line x1="31" y1="12" x2="31" y2="36" stroke-width="2" stroke="currentColor"/>
@@ -19,9 +33,9 @@ miro.onReady(() => {
 
 const svgIconStageTemplate = `
 <svg viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg">
-<line x1="0" y1="11" x2="48" y2="11" stroke-width="2" stroke="#555"/>
-<line x1="15" y1="12" x2="15" y2="48" stroke-width="2" stroke="#555"/>
-<line x1="31" y1="12" x2="31" y2="48" stroke-width="2" stroke="#555"/>
+<line x1="0" y1="11" x2="48" y2="11" stroke-width="2" stroke="${colorScheme.dark}"/>
+<line x1="15" y1="12" x2="15" y2="48" stroke-width="2" stroke="${colorScheme.dark}"/>
+<line x1="31" y1="12" x2="31" y2="48" stroke-width="2" stroke="${colorScheme.dark}"/>
 <rect x="16" y="12" width="14" height="36" rx="0" stroke-width="0" fill="{fillColor}" stroke="currentColor"/>
 </svg>
 `;
@@ -30,10 +44,10 @@ const svgIconStageHighlight = svgIconStageTemplate.replace('{fillColor}', '#7777
 
 const svgIconWorkItemTemplate = `
 <svg viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg">
-<line x1="0" y1="11" x2="48" y2="11" stroke-width="2" stroke="#999"/>
-<line x1="15" y1="12" x2="15" y2="48" stroke-width="2" stroke="#999"/>
-<line x1="31" y1="12" x2="31" y2="48" stroke-width="2" stroke="#999"/>
-<rect x="6" y="10" width="36" height="24" rx="0" stroke-width="2" stroke="#000" fill="{fillColor}" transform="rotate(-12 24 24)"/>
+<line x1="0" y1="11" x2="48" y2="11" stroke-width="2" stroke="${colorScheme.medium}"/>
+<line x1="15" y1="12" x2="15" y2="48" stroke-width="2" stroke="${colorScheme.medium}"/>
+<line x1="31" y1="12" x2="31" y2="48" stroke-width="2" stroke="${colorScheme.medium}"/>
+<rect x="6" y="10" width="36" height="24" rx="0" stroke-width="2" stroke="${colorScheme.default}" fill="{fillColor}" transform="rotate(-12 24 24)"/>
 </svg>
 `;
 const svgIconWorkItemNormal = svgIconWorkItemTemplate.replace('{fillColor}', '#cecece');
